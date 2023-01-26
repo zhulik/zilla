aeaac01859ea4eedb8dff5cbbbca74b3580891e3
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2023-01-26 15:25:10 +0100

    Initial commit

---
+++ lib/async/openapi.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "openapi/version"
+
+module Async
+  module Openapi
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
